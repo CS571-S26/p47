09a32fdbb2f70533dfef20aec75d6ff582ab8017
Fritz Brooks <fbrooks905@gmail.com>
2026-03-16 19:58:39 -0500

    fixing deploy bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'node:fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'write-nojekyll',
+      closeBundle() {
+        writeFileSync('docs/.nojekyll', '')
+      },
+    },
+  ],
+  base: '/p47/',
+  build: {
+    outDir: 'docs',
+  },
 })
