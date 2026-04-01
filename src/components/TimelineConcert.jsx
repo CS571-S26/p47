ebdc2295fe108ada91b4dd3e5cd05116993a49d4
RyanVauk <ryan.vauk@gmail.com>
2026-04-01 17:03:09 -0500

    Fixed issue where design was not responsive

--- src/components/TimelineConcert.jsx
+++ src/components/TimelineConcert.jsx
@@ -1,4 +1,6 @@
 import { Clock } from 'lucide-react'
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 import './TimelineConcert.css'
 
@@ -13,44 +15,51 @@ function TimelineConcert({ concert }) {
     const year = date.getFullYear()
 
     return (
-        <div className="timeline-concert-card">
-            <div className="timeline-concert-date-card">
-                <div className="timeline-concert-date-month">{month}</div>
-                <div className="timeline-concert-date-day">{day}</div>
-                <div className="timeline-concert-date-year">{year}</div>
-            </div>
-
-            <img
-                src={concert.image}
-                alt={concert.artist}
-                className="timeline-concert-image"
-            />
-            <div>
-                <div className="timeline-concert-artist">{concert.artist}</div>
-                <div className="timeline-concert-venue">{concert.venue} • {concert.city}</div>
-
-                <div className="timeline-concert-rating-row">
-                    <div className='timeline-concert-genre'>{concert.genre}</div>
-                    <div className="timeline-concert-stars">
-                        {'★'.repeat(concert.rating)}
-                        {'☆'.repeat(5 - concert.rating)}
+        <Container className="timeline-concert-card">
+            <Row>
+                <Col xs="auto">
+                    <div className="timeline-concert-date-card">
+                        <div className="timeline-concert-date-month">{month}</div>
+                        <div className="timeline-concert-date-day">{day}</div>
+                        <div className="timeline-concert-date-year">{year}</div>
                     </div>
-                    <div className="timeline-concert-rating">{concert.rating}.0</div>
-                </div>
+                </Col>
 
-                <div className="timeline-concert-stats-row">
-                    <Clock size={16} />
-                    <div className="timeline-concert-songcount">{concert.songCount} songs</div>
+                <Col xs="auto">
+                    <img
+                        src={concert.image}
+                        alt={concert.artist}
+                        className="timeline-concert-image"
+                    />
+                </Col>
 
-                    <button className="timeline-concert-view-details">
-                        View Details
-                    </button>
-                </div>
+                <Col>
+                    <div className="timeline-concert-artist">{concert.artist}</div>
+                    <div className="timeline-concert-venue">{concert.venue} • {concert.city}</div>
 
-            </div>
+                    <div className="timeline-concert-rating-row">
+                        <div className='timeline-concert-genre'>{concert.genre}</div>
+                        <div className="timeline-concert-stars">
+                            {'★'.repeat(concert.rating)}
+                            {'☆'.repeat(5 - concert.rating)}
+                        </div>
+                        <div className="timeline-concert-rating">{concert.rating}.0</div>
+                    </div>
+
+                    <div className="timeline-concert-stats-row">
+                        <Clock size={16} />
+                        <div className="timeline-concert-songcount">{concert.songCount} songs</div>
+
+                        <button className="timeline-concert-view-details">
+                            View Details
+                        </button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
 
 
-        </div>
+
 
     )
 }
