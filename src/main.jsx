9a484b578ed9a826fd5f0a898f2dcdd3ffa85681
RyanVauk <ryan.vauk@gmail.com>
2026-04-01 13:47:58 -0500

    Map page now has a workable map

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'leaflet/dist/leaflet.css'
+import './leaflet-overrides.css'
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
--- src/pages/MapsPage.jsx
+++ src/pages/MapsPage.jsx
@@ -1,11 +1,20 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+import './MapsPage.css'
+
 function MapsPage() {
   return (
-    <section id="center">
-      <div>
-        <h1>Maps</h1>
-        <p>Concert locations and venue views will live here soon.</p>
+    <div className="map-page">
+      <h1>Map</h1>
+      <p>Your concerts on a map (coming soon).</p>
+      <div className="map-box">
+        <MapContainer center={[39.5, -98.35]} zoom={4} scrollWheelZoom>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
       </div>
-    </section>
+    </div>
   )
 }
 
