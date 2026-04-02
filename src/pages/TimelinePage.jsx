d1e3ff0bc4e32166eeba8cb78042236039e6cfda
RyanVauk <ryan.vauk@gmail.com>
2026-04-01 22:03:04 -0500

    Added log concert button on timeline page

--- src/pages/TimelinePage.jsx
+++ src/pages/TimelinePage.jsx
@@ -1,18 +1,35 @@
-import { concerts } from '../data/MockConcerts'
 import TimelineConcert from '../components/TimelineConcert'
 import TimelineStats from '../components/TimelineStats'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Plus } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
+
+import { concerts } from '../data/MockConcerts'
 
 function TimelinePage() {
   return (
     <Container fluid style={{ padding: "1rem" }}>
       <Row>
         <Col md={2}>
-          <TimelineStats/>
+          <TimelineStats />
         </Col>
-        <Col md={8}>
-        <div style={{fontSize: "48px", fontWeight: "700"}}>My Concert Timeline</div>
-        <div style={{fontSize: "24px", fontWeight: "300", marginBottom: "15px"}}>Your logged shows, newest first</div>
+        <Col>
+          <Row style={{ alignItems: "center", justifyContent: "space-between"}}>
+            <Col xs="auto">
+              <div style={{ fontSize: "48px", fontWeight: "700" }}>My Concert Timeline</div>
+            </Col>
+            <Col xs="auto">
+              <Button
+                as={NavLink}
+                to="/add-concert"
+                style={{ padding: "6px 12px", fontSize: "16px", fontWeight: "700", marginLeft: "auto", height: "fit-content" }}
+              >
+                <Plus size={18} /> Log a New Show
+              </Button>
+            </Col>
+          </Row>
+
+          <div style={{ fontSize: "24px", fontWeight: "300", marginBottom: "15px" }}>Your logged shows, newest first</div>
           {concerts.map((concert) => (
             <TimelineConcert key={concert.id} concert={concert} />
           ))}
